89c4e528e47bd6b25eb8e414d4f8635e4a2f326b
Dana <144537942+danad1821@users.noreply.github.com>
2024-02-29 15:16:36 +0200

    Updating Navbar

--- main.js
+++ main.js
@@ -1,105 +1,108 @@
-let user = ["dana", "pass"]
-//opens the account dropdown
-function displayAccountInfo() {
-    let account = document.getElementsByClassName("account");
-    account[0].style.display = "flex"
-    account[0].style.flexDirection = "column"
-    document.getElementById("nav-bar").style.width = "100%"
-}
-//closes the account drop-down
-function closeAccountInfo() {
-    let account = document.getElementsByClassName("account");
-    account[0].style.display = "none"
-}
-//account dropdown for big screens
+$(document).ready(function () {
 
-let accountBtn = document.getElementById("account-icon")
-accountBtn.addEventListener("click", () => {
-    let account = document.getElementById("account")
-    if (account.style.display == "none") {
-        displayAccountInfo();
-        if (window.sessionStorage.getItem("SignedIn") == "true") {
-            document.getElementsByClassName("signed-out")[0].style.display = "none";
-            let cur = document.getElementsByClassName("account-info")[0]
-            cur.style.display = "flex";
-            cur.style.flexDirection = "column";
-            document.getElementById("username-text").innerText = user[0];
+    // Open account dropdown
+    function displayAccountInfo() {
+        $("#account").css({position: 'absolute', right: '-220px'});
+        $("#account").animate({
+            right: '0'
+        }, "slow", function(){
+            $("body").css({overflowX: 'hidden'})
+        })
+        $("#account").css({ display: "flex", flexDirection: "column" });
+        $("#nav-bar").width("100%");
+    }
+
+    // Close account dropdown
+    function closeAccountInfo() {
+        $("#account").animate({
+          right: "-220px" // Move element 120px to the right
+        }, "slow", function() {
+            $("body").css({overflowX: 'hidden'});
+          // Callback function after animation completes
+          $("#account").hide();
+        });
+      }      
+
+    // Account dropdown for big screens
+    $("#account-icon").click(function () {
+        let account = $("#account");
+        if (account.css("display") === "none") {
+            displayAccountInfo();
+
+        } else {
+            closeAccountInfo();
         }
-    }
-    else {
-        closeAccountInfo()
-    }
-})
-
-
-//dropdown for small screens
-let pressedDrop = false;
-let dropDown = document.getElementsByClassName("drop-nav-bar");
-dropDown[0].addEventListener("click", () => {
-    if (pressedDrop == false) {
-        let navItems = document.getElementsByClassName("nav-items");
-        navItems[0].style.display = "block";
-        if (window.sessionStorage.getItem("SignedIn") == "true") {
-            document.getElementsByClassName("signed-out")[1].style.display = "none";
-            let cur = document.getElementsByClassName("account-info")[1]
-            cur.style.display = "flex";
-            cur.style.flexDirection = "column";
-            document.getElementById("username-text2").innerText = user[0];
-        }
-        pressedDrop = true;
-    }
-    else {
-        let navItems = document.getElementsByClassName("nav-items");
-        navItems[0].style.display = "none";
-        pressedDrop = false;
-    }
-})
-//sign out for nav on big screens
-let btnSignOut = document.getElementById("sign-out-btn")
-btnSignOut.addEventListener("click", function () {
-    window.sessionStorage.setItem("SignedIn", "false");
-    window.sessionStorage.removeItem("order");
-    window.sessionStorage.removeItem("totalPrice");
-    document.getElementById("account").style.display = "none";
-    location.reload()
-    document.getElementsByClassName("signed-out")[0].style.display = "block";
-    document.getElementsByClassName("account-info")[0].style.display = "none";
-
-})
-//sign out for nav on small screens
-let btnSignOut2 = document.getElementById("sign-out-btn2")
-btnSignOut2.addEventListener("click", function () {
-    window.sessionStorage.setItem("SignedIn", "false");
-    window.sessionStorage.removeItem("order");
-    window.sessionStorage.removeItem("totalPrice");
-    let navItems = document.getElementsByClassName("nav-items");
-    navItems[0].style.display = "none";
-    location.reload()
-    document.getElementsByClassName("signed-out")[1].style.display = "block";
-    document.getElementsByClassName("account-info")[1].style.display = "none";
-})
-//used for smooth transitions between pages
-const swup = new Swup();
-
-$(document).ready(function(){
-    let loginPopup = $(".l");
-    let signupPopup=$(".s")
-    let loginBtn = $(".login");
-    let signupBtn=$(".up");
-    let signinX = $(".signX");
-    let backdrop = $("#backdrop"); 
-    
-    // Show login popup and backdrop
-    loginBtn.on("click",function(){
-      signupPopup.hide();
-      loginPopup.show();
-      backdrop.show();
     });
-  
-    signupBtn.on("click",function(){
-      loginPopup.hide();
-      signupPopup.show();
-      backdrop.show();
+
+    // Dropdown for small screens
+    let pressedDrop = false;
+    $(".drop-nav-bar").click(function () {
+        if (!pressedDrop) {
+            $(".nav-items").css({position: 'absolute', right: '-220px'});
+            $(".nav-items").animate({
+                right: '0'
+            }, "slow", function(){
+                $("body").css({overflowX: 'hidden'})
+            })
+            $(".nav-items").css({ display: "flex", flexDirection: "column" });
+            $(".nav-items").show();
+            if (window.sessionStorage.getItem("SignedIn") === "true") {
+                $(".signed-out:last").hide();
+                $(".account-info:last").css({ display: "flex", flexDirection: "column" });
+                $("#username-text2").text(user[0]);
+            }
+            pressedDrop = true;
+        } else {
+            $(".nav-items").animate({
+                right: "-220px" // Move element 220px to the right
+              }, "slow", function() {
+                  $("body").css({overflowX: 'hidden'});
+                // Callback function after animation completes
+                $(".nav-items").hide();
+              });
+            pressedDrop = false;
+        }
+    });
+
+    // Sign out for nav on big screens
+    $("#sign-out-btn").click(function () {
+        $("#account").hide();
+        location.reload();
+        $(".signed-out:first").show();
+        $(".account-info:first").hide();
+    });
+
+    // Sign out for nav on small screens
+    $("#sign-out-btn2").click(function () {
+        $(".nav-items:first").hide();
+        location.reload();
+        $(".signed-out:last").show();
+        $(".account-info:last").hide();
+    });
+
+
+    // Swup instance (assuming Swup is already loaded as a separate script)
+    const swup = new Swup();
+
+
+    let loginPopup = $(".l");
+    let signupPopup = $(".s")
+    let loginBtn = $(".login");
+    let signupBtn = $(".up");
+    let signinX = $(".signX");
+    let backdrop = $("#backdrop");
+
+    // Show login popup and backdrop
+    loginBtn.on("click", function () {
+        signupPopup.hide();
+        loginPopup.show();
+        backdrop.show();
+    });
+
+    signupBtn.on("click", function () {
+        loginPopup.hide();
+        signupPopup.show();
+        backdrop.show();
     });
 
     /*Function to Remove Error Message*/
@@ -109,36 +112,37 @@ $(document).ready(function(){
         let alertMessage = parentElement.find('small');
         alertMessage.text('');
     }
-    
+
     // Hide login popup and backdrop
     signinX.click(function () {
-      loginPopup.hide();
-      signupPopup.hide();
-      backdrop.hide();
-      $("#sign-in-user").val("");
-      $("#sign-in-pass").val("");
-      $("#frst-name-Input").val("");
-      $("#lst-name-Input").val("");
-      $("#ContactInput").val("");
-      $("#bday").val("");
-      $("#password").val("");
-      $("#confirmPassword").val("");
-      $("#agree").prop("checked", false);
-      $(".policy .paragraph").show();
-      removeError($("#sign-in-user"));
-      removeError($("#sign-in-pass"));
-      removeError($("#frst-name-Input"));
-      removeError($("#lst-name-Input"));
-      removeError($("#ContactInput"));
-      removeError($("#bday"));
-      removeError($("#password"));
-      removeError($("#confirmPassword"));
-      $(".policy").removeClass("error");
-      $("#policyError").text("");
+        loginPopup.hide();
+        signupPopup.hide();
+        backdrop.hide();
+        $("#sign-in-user").val("");
+        $("#sign-in-pass").val("");
+        $("#frst-name-Input").val("");
+        $("#lst-name-Input").val("");
+        $("#ContactInput").val("");
+        $("#bday").val("");
+        $("#password").val("");
+        $("#confirmPassword").val("");
+        $("#agree").prop("checked", false);
+        $(".policy .paragraph").show();
+        removeError($("#sign-in-user"));
+        removeError($("#sign-in-pass"));
+        removeError($("#frst-name-Input"));
+        removeError($("#lst-name-Input"));
+        removeError($("#ContactInput"));
+        removeError($("#bday"));
+        removeError($("#password"));
+        removeError($("#confirmPassword"));
+        $(".policy").removeClass("error");
+        $("#policyError").text("");
     });
 
-    $(".togglePassword").on("click",function(){
+    $(".togglePassword").on("click", function () {
         // Toggle the eye icon class to switch between open and closed eye icons
         $(this).toggleClass('far fa-eye far fa-eye-slash');
     });
-})
+
+});
--- navbar.php
+++ navbar.php
@@ -10,30 +10,33 @@ echo"
         <a href='../Menu/menu.php' class='nav-options'>Menu</a>
         <a href='../Contact/contact.php' class='nav-options'>Contact Us</a>
         <a href='../Review/review.php' class='nav-options'>Leave a review</a>
-        <svg class='account-icon account-icons' id='account-icon' xmlns='http://www.w3.org/2000/svg' height='1em'
-            viewBox='0 0 448 512'>
-            <path
-                d='M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z' />
+        <svg class='account-icon account-icons' id='account-icon' xmlns='http://www.w3.org/2000/svg' width='35px' height='35px' fill='currentColor' class='bi bi-list' viewBox='0 0 20 20'>
+            <path fill-rule='evenodd' d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5'/>
         </svg>
         <div class='account' id='account'>
-            <ul class='signed-out account-section'>
-                <li><a href='#' class='login'>Log In</a></li>
-                <li><a href='#' class='up'>Sign Up</a></li>
-            </ul>
-            <ul class='account-info account-section'>
-                <li>
-                    <div class='account-info_div'>
-                        <h4 id='username-text'>Username</h4>
-                        <a href='../Cart/cart.php' class='cart-btn'>
-                            <svg class='account-icons' class='cart-icon' xmlns='http://www.w3.org/2000/svg' height='1em'
-                                viewBox='0 0 576 512'>
-                                <path
-                                    d='M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z' />
-                            </svg>
-                        </a>
-                    </div>
-                </li>
-                <li><button id='sign-out-btn'>Sign Out</button></li>
+            <ul class='main-ul'>";
+            if(!isset($_SESSION['account'])){
+                echo "
+                <li><a href='#' class='login signing-btns'>Log In</a></li>
+                <li><a href='#' class='up signing-btns'>Sign Up</a></li>
+                ";
+            }
+            else{
+                echo "<li>
+                <div class='account-info_div'>
+                    <h4 id='username-text'>Username</h4>
+                    <a href='../Cart/cart.php' class='cart-btn'>
+                        <svg class='account-icons' class='cart-icon' xmlns='http://www.w3.org/2000/svg' height='1em'
+                            viewBox='0 0 576 512'>
+                            <path
+                                d='M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z' />
+                        </svg>
+                    </a>
+                </div>
+            </li>
+            <li><button id='sign-out-btn'>Sign Out</button></li>";
+            }
+            echo "
             </ul>
         </div>
     </nav>
@@ -46,18 +49,19 @@ echo"
         </button>
         <div class='nav-items'>
             <ul>
-                <li><a href='../Home/index.php' class='nav-options-nav2'>Home</a></li>
-                <li><a href='../About/about.php' class='nav-options-nav2'>About Us</a></li>
-                <li><a href='../Menu/menu.php' class='nav-options-nav2'>Menu</a></li>
-                <li><a href='../Contact/contact.php' class='nav-options-nav2'>Contact Us</a></li>
-                <li><a href='../Review/review.php' class='nav-options-nav2'>Leave a review</a></li>
-            </ul>
-            <ul class='signed-out account-section'>
-                <li><a href='#' class='login'>Log In</a></li>
-                <li><a href='#' class='up'>Sign Up</a></li>
-            </ul>
-            <ul class='account-info account-section'>
-                <li>
+                <li class='nav-2-li'><a href='../Home/index.php' class='nav-options-nav2'>Home</a></li>
+                <li class='nav-2-li'><a href='../About/about.php' class='nav-options-nav2'>About Us</a></li>
+                <li class='nav-2-li'><a href='../Menu/menu.php' class='nav-options-nav2'>Menu</a></li>
+                <li class='nav-2-li'><a href='../Contact/contact.php' class='nav-options-nav2'>Contact Us</a></li>
+                <li id='last'><a href='../Review/review.php' class='nav-options-nav2'>Leave a review</a></li>";
+                if(!isset($_SESSION['account'])){
+                    echo "
+                    <li><a href='#' class='login signing-btns'>Log In</a></li>
+                    <li><a href='#' class='up signing-btns'>Sign Up</a></li>
+                    ";
+                }
+                else{
+                    echo "<li>
                     <div class='account-info_div'>
                         <h4 id='username-text2'>Username</h4>
                         <a href='../Cart/cart.php' class='cart-btn'>
@@ -69,8 +73,12 @@ echo"
                         </a>
                     </div>
                 </li>
-                <li><button id='sign-out-btn2'>Sign Out</button></li>
+                <li><button id='sign-out-btn2'>Sign Out</button></li>";
+                }
+                
+                echo"
             </ul>
+            
         </div>
     </nav>
 ";
