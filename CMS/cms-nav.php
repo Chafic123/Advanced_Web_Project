24cca1d9a0172c3ed90278f675ba5db4d6988965
danad1821 <danadabdoub@gmail.com>
2024-03-18 20:13:50 +0200

    cms navbar

--- CMS/cms-nav.php
+++ CMS/cms-nav.php
@@ -1,26 +1,27 @@
 <?php
 echo '<!-- Navigation Bar -->
-<nav class="navbar navbar-expand-lg" id="nav">
-  <a class="navbar-brand" href="cms.php"><img src="../Malaz Design/Nav  Bar Logo - Not Bold.png" alt="Logo" id="logo"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="navbarItem">
-      <li class="nav-item active">
-        <a class="nav-link" href="cms.php">Dashboard</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="cms-menu.php">Menu</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="cms-review.php">Reviews</a>
-      </li>
-    </ul>
-    <span class="navbar-text">
-        <button class="sign-out">Sign Out</button>
-    </span>
-  </div>
-</nav>';
+<nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="cms.php">Dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="cms-review.php">Reviews</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Menu
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Add to Menu</a></li>
+                            <li><a class="dropdown-item" href="#">Edit Menu</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>';
 ?>
--- CMS/cms-review.php
+++ CMS/cms-review.php
@@ -6,17 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Malaz</title>
     <link rel="icon" href="../Malaz Design/Malaz---icon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="cms.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/tw-cen-mt-std" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="cms.css">
 </head>
 
 <body>
     <?php
-    include("../config.php");
+        include("../config.php");
+        include "cms-nav.php";
     ?>
     <div style="text-align: center;">
         <h1>Reviews</h1>
