f8f72c3e08ffd2c15e23f6313d4e9d34daffd3a0
Dana <144537942+danad1821@users.noreply.github.com>
2024-04-02 18:34:48 +0300

    another update

--- CMS/edit-menu.php
+++ CMS/edit-menu.php
@@ -5,7 +5,6 @@ if(session_status()!=2){
 if(isset($_SESSION['admin']) && $_SESSION['admin']==true){ 
 
     // Check if form is submitted
-    if (isset($_POST["edit"])) {
         // Include database configuration
         include '../config.php';
 
@@ -169,7 +168,6 @@ if(isset($_SESSION['admin']) && $_SESSION['admin']==true){
             }
         }
         header("Location: cms-viewmenu.php");
-    }
 }else{
     header("Location: ../Home/index.php");
 }
